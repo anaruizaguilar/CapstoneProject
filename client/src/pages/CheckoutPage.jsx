0f5c41e0a5dc8fb1f5f9c0336691b01f56ab5fc2
Ana Ruiz <aruiz_90@hotmail.com>
2024-04-11 18:02:03 -0500

    can now type into dummy checkout page

--- client/src/pages/CheckoutPage.jsx
+++ client/src/pages/CheckoutPage.jsx
@@ -1,32 +1,29 @@
-import { useState } from "react";
-import Cart from "../components/Cart";
+import { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom'
 
 function CheckoutPage() {
     
     const navigate = useNavigate();
 
-    const [formData, setFormData] = useState({
-        firstname: "",
-        lastname: "",
-        shippingAddress: "",
-        billingAddress: "",
-        creditCardNumber: "",
-        nameOnCard: "",
-        expirationDate: "",
-    });
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [shippingAddress, setShippingAddress] = useState("");
+    const [billingAddress, setBillingAddress] = useState("");
+    const [creditCardNumber, setCreditCardNumber] = useState("");
+    const [nameOnCard, setNameOnCard] = useState("");
+    const [expirationDate, setExpirationDate] = useState("");
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
-    }
 
-    const handleSubmit = (e) => {
+async function handleSubmit(e) {
         e.preventDefault();
-        console.log(formData);
+        console.log(firstname, lastname, shippingAddress, billingAddress, creditCardNumber, nameOnCard, expirationDate);
+        setFirstname("");
+        setLastname("");
+        setShippingAddress("");
+        setBillingAddress("");
+        setCreditCardNumber("");
+        setNameOnCard("");
+        setExpirationDate("");
     }
 
     return(
@@ -34,25 +31,25 @@ function CheckoutPage() {
             <h2>Checkout</h2>
             <form className="checkout-form">
                 <label>
-                    Firstname: {""} <input value={formData.firstname} onChange={handleChange}/>
+                    Firstname: {""} <input value={firstname} onChange={(e) => {setFirstname(e.target.value);}}/>
                 </label>
                 <label>
-                    Lastname: {""} <input value={formData.lastname} onChange={handleChange} />
+                    Lastname: {""} <input value={lastname} onChange={(e) => {setLastname(e.target.value);}} />
                 </label>
                 <label>
-                   Shipping Address: {""} <input value={formData.shippingAddress} onChange={handleChange}/>
+                   Shipping Address: {""} <input value={shippingAddress} onChange={(e) => {setShippingAddress(e.target.value);}}/>
                 </label>
                 <label>
-                    Billing Address: {""} <input value={formData.billingAddress} onChange={handleChange}/>
+                    Billing Address: {""} <input value={billingAddress} onChange={(e) => {setBillingAddress(e.target.value);}}/>
                 </label>
                 <label>
-                    Credit Card Number: {""} <input value={formData.creditCardNumber} onChange={handleChange}/>
+                    Credit Card Number: {""} <input value={creditCardNumber} onChange={(e) => {setCreditCardNumber(e.target.value);}}/>
                 </label>
                 <label>
-                    Name On Card: {""} <input value={formData.nameOnCard} onChange={handleChange}/>
+                    Name On Card: {""} <input value={nameOnCard} onChange={(e) => {setNameOnCard(e.target.value);}}/>
                 </label>
                 <label>
-                    Expiration Date: {""} <input value={formData.expirationDate} onChange={handleChange}/>
+                    Expiration Date: {""} <input value={expirationDate} onChange={(e) => {setExpirationDate(e.target.value);}}/>
                 </label>
                 <button type="checkout-submit-button" onClick={handleSubmit}>Submit</button>
                 <button className="checkout-return-button" onClick={() => navigate('/')}>Continue Shopping</button>
--- server/db.js
+++ server/db.js
@@ -1,9 +1,15 @@
 const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/fake_store_db');
+const uuid = require('uuid');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET || 'password';
 
 const createTables = async() => {
     const SQL = `
     DROP TABLE IF EXISTS customers;
+    DROP TABLE IF EXISTS products;
+    DROP TABLE IF EXISTS cart;
     CREATE TABLE customers(
         id UUID PRIMARY KEY,
         firstname VARCHAR(50),
@@ -11,6 +17,14 @@ const createTables = async() => {
         username VARCHAR(50) UNIQUE NOT NULL,
         password VARCHAR(100) NOT NULL
     );
+    CREATE TABLE products(
+        id UUID PRIMARY KEY,
+        name VARCHAR(100),
+        description VARCHAR(200),
+        price DECIMAL(20,2),
+        image VARCHAR(100)
+    );
+    
     `;
     await client.query(SQL);
 };
